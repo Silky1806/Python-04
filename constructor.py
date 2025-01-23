4982780fa2321224de8c3b5c113002edd442ada4
Silky Nigam <126972307+Silky1806@users.noreply.github.com>
2025-01-23 16:06:51 +0530

    Add files via upload

---
+++ constructor.py
@@ -0,0 +1,8 @@
+class car:
+    def __init__(self,model,year):
+        self.model=model
+        self.year=year
+    def display(s):
+        print(s.model,s.year)
+c1=car("xuv",2000)
+c1.display()
